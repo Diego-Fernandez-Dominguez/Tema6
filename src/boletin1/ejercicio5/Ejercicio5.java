66179cc4d567e7fb4d1d6da2697145e0266bc0ba
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-23 10:16:40 +0200

    Ejercicio 5 acabado

--- src/boletin1/ejercicio5/Ejercicio5.java
+++ src/boletin1/ejercicio5/Ejercicio5.java
@@ -1,40 +1,38 @@
-package boletin1.ejercicio4;
+package boletin1.ejercicio5;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Ejercicio4 {
+public class Ejercicio5 {
 	public static void main(String[] args) {
 
+		final String FICHERO = "src\\boletin1\\ejercicio5\\datos.txt";
 		String cad = "";
+
+		int num = 0;
 
 		Scanner reader = new Scanner(System.in);
 
-		try (BufferedWriter esc = new BufferedWriter(new FileWriter("src\\boletin1\\ejercicio4\\Texto4.txt"))) {
+		try (BufferedWriter esc = new BufferedWriter(new FileWriter(FICHERO, true))) {
 
-			System.out.println("Dime el texto");
+			System.out.println("Dime tu nombre");
 			cad = reader.nextLine();
 
-			do {
+			System.out.println("Dime tu edad");
+			num = reader.nextInt();
 
-				esc.write(cad);
-				esc.newLine();
+			esc.write(cad + " " + num);
 
-				System.out.println("Dime el texto");
-				cad = reader.nextLine();
+			esc.newLine();
 
-			} while (!cad.equalsIgnoreCase("fin"));
-			
 			esc.flush();
 
 		} catch (IOException e) {
 			System.err.println("Error de escritura en el fichero: " + e.getMessage());
-		} 
-		
+		}
 		reader.close();
-
 	}
 
 }
